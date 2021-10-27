214f3e60c53ccceea737f9485f9a6a3aa646f5a2
Derek Louis <derek.louis@avanade.com>
2021-10-27 15:15:28 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularPlayground';
+
+  @ViewChild('AM') AM!: ElementRef<HTMLCanvasElement>;
+
+  BG() {
+    this.AM.nativeElement.style.backgroundImage = `linear-gradient(${Math.ceil(
+      Math.random() * 360
+    )}deg, rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(
+      Math.random() * 255
+    )}, ${Math.ceil(Math.random() * 255)}), rgb(${Math.ceil(
+      Math.random() * 255
+    )}, ${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)}))`;
+  }
+
+  private ctx!: CanvasRenderingContext2D | null;
+
+  circle() {
+    this!.ctx!.beginPath();
+    this!.ctx!.arc(
+      Math.ceil(Math.random() * window.innerWidth),
+      Math.ceil(Math.random() * window.innerHeight),
+      Math.ceil(Math.random() * window.innerWidth) / 2,
+      0,
+      2 * Math.PI
+    );
+    this!.ctx!.strokeStyle = `rgb(${Math.ceil(
+      Math.random() * 255
+    )}, ${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)})`;
+    this!.ctx!.lineWidth = Math.ceil(Math.random() * 15);
+    this!.ctx!.stroke();
+  }
+
+  circleFilled() {
+    this!.ctx!.beginPath();
+    this!.ctx!.arc(
+      Math.ceil(Math.random() * window.innerWidth),
+      Math.ceil(Math.random() * window.innerHeight),
+      Math.ceil(Math.random() * window.innerWidth) / 2,
+      0,
+      2 * Math.PI
+    );
+    this!.ctx!.fillStyle = `rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(
+      Math.random() * 255
+    )}, ${Math.ceil(Math.random() * 255)})`;
+    this!.ctx!.fill();
+  }
+
+  reset() {
+    this!.ctx!.clearRect(
+      0,
+      0,
+      this.AM.nativeElement.width,
+      this.AM.nativeElement.height
+    );
+  }
+
+  @ViewChild('lid') lid!: ElementRef;
+  @ViewChild('toolBar') toolBar!: ElementRef;
+
+  hover() {
+    this.lid.nativeElement.className = 'lidLift';
+  }
+
+  endHover() {
+    this.lid.nativeElement.className = '';
+  }
+
+  myIntId: any = 'default';
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    this.ctx = this.AM.nativeElement.getContext('2d');
+    this.AM.nativeElement.width = window.innerWidth;
+    this.AM.nativeElement.height = window.innerHeight;
+    this.BG();
+    let canvasCopy = this.AM.nativeElement;
+    window.addEventListener('resize', function () {
+      canvasCopy.width = window.innerWidth;
+      canvasCopy.height = window.innerHeight;
+    });
+    this.myIntId = setInterval(() => {
+      this.circleFilled();
+    }, 500);
+  }
+
+  updateTitle(event: boolean) {
+    clearInterval(this.myIntId);
+    this.reset();
+    this.toolBar.nativeElement.className = 'toolBarVisible';
+  }
+
+  constructor() {}
 }
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,86 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css'],
+})
+export class TitleComponent implements OnInit {
+  constructor() {}
+
+  @ViewChild('T1') T1!: ElementRef;
+  @ViewChild('T2') T2!: ElementRef;
+  @ViewChild('T3') T3!: ElementRef;
+  @ViewChild('T4') T4!: ElementRef;
+  @ViewChild('T5') T5!: ElementRef;
+  @ViewChild('T6') T6!: ElementRef;
+  @ViewChild('T7') T7!: ElementRef;
+  @ViewChild('T8') T8!: ElementRef;
+  @ViewChild('T9') T9!: ElementRef;
+  @ViewChild('T10') T10!: ElementRef;
+  @ViewChild('T11') T11!: ElementRef;
+  @ViewChild('titleWrapper') titleWrapper!: ElementRef;
+
+  explode() {
+    this.titleWrapper.nativeElement.className = 'TWHidden';
+  }
+
+  @Output() titleActive = new EventEmitter<boolean>();
+
+  killTitle() {
+    this.explode();
+    this.titleActive.emit(false);
+  }
+
+  finishTitle!: Function;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    let titleArray = [
+      this.T1.nativeElement,
+      this.T2.nativeElement,
+      this.T3.nativeElement,
+      this.T4.nativeElement,
+      this.T5.nativeElement,
+      this.T6.nativeElement,
+      this.T7.nativeElement,
+      this.T8.nativeElement,
+      this.T9.nativeElement,
+      this.T10.nativeElement,
+      this.T11.nativeElement,
+    ];
+
+    for (let letter of titleArray) {
+      letter.style.color = `rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(
+        Math.random() * 255
+      )}, ${Math.ceil(Math.random() * 255)})`;
+      if (letter.id !== 'T6') {
+        letter.style.animation = `letterFloat ${Math.ceil(
+          Math.random() * 3
+        )}s ease-out 0s infinite alternate forwards`;
+      }
+    }
+    const TWCopy = this.titleWrapper;
+    const finishTitle = function (e: any) {
+      if (e.propertyName === 'transform') {
+        TWCopy.nativeElement.style.display = 'none';
+      }
+    };
+
+    this.titleWrapper.nativeElement.addEventListener(
+      'transitionend',
+      finishTitle
+    );
+  }
+}
